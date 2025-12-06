5809a6cacb633eb82ee8a01c6e31c745772fa0b2
Vishwanath Archak M R <vishwanath05122006@gmail.com>
2025-12-06 23:26:38 +0530

    Add index.ts barrel export for maps module

---
+++ src/components/maps/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Soul Trail - Maps Module Exports
+ * @author Vishwanath Archak
+ */
+
+export { LeafletMap } from './LeafletMap';
+export { GoogleMapView } from './GoogleMapView';
+export { MapMarker } from './MapMarker';
+
+export type { LocationCoords, Place, MapProps, GoogleMapProps } from '../types/maps';
