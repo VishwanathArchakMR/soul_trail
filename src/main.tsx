a8c2b547f76cb60b31b907c8e28c2833f268bf84
Vishwanath Archak M R <vishwanath05122006@gmail.com>
2025-12-06 23:06:56 +0530

    Add authorship comments to main.tsx
    Added documentation comments for the main application entry point.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+/**
+ * Soul Trail Application Entry Point
+ * @author Vishwanath Archak
+ * @description React application bootstrap and DOM mount
+ */
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
